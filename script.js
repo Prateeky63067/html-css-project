66ac531580764b0fc6388e74646894899a4b06b4
Prateek yadav <shubhamy9451@gmail.com>
2021-11-04 14:34:30 +0530

    first html css project

---
+++ script.js
@@ -0,0 +1,9 @@
+function openmenu()
+{
+    document.getElementById("navbar").style.left= "0px";
+    document.getElementById("navbar").style.bottom= "1000px";
+}
+function closemenu()
+{
+    document.getElementById("navbar").style.left= "-280px";
+}
